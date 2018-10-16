aaac21d70d7d3d16b0470bcca188a7efeabfdf9b
WenChuanmeng <1124775340@qq.com>
2018-10-16 17:27:49 +0800

    初始化项目

---
+++ src/main/java/com/sr/searchroom/SearchRoomApplication.java
@@ -0,0 +1,12 @@
+package com.sr.searchroom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SearchRoomApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SearchRoomApplication.class, args);
+    }
+}
