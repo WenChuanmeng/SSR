d329b27813a2bb47579836dd69ff9b633988e89d
WenChuanmeng <1124775340@qq.com>
2018-10-23 10:08:02 +0800

    图标

--- src/main/java/com/sr/searchroom/web/controller/HomeController.java
+++ src/main/java/com/sr/searchroom/web/controller/HomeController.java
@@ -1,18 +1,25 @@
 package com.sr.searchroom.web.controller;
 
+import com.sr.searchroom.base.ApiResponse;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
-@RequestMapping(value = "/api/index")
-public class IndexController {
+public class HomeController {
 
     @RequestMapping(value = "/index")
     public String index(Model model) {
         model.addAttribute("title","欢迎欢迎");
         model.addAttribute("name","张三ABCdef123");
         return "index";
+    }
+
+    @GetMapping(value = "/get")
+    @ResponseBody
+    public ApiResponse get() {
+        return ApiResponse.ofMessage(200, "返回");
     }
 }
